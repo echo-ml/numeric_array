319d5824c4eea6c7647767f2152783362dbe8f55
Ribulous Barnulous <ribulous.barnulous@gmail.com>
2014-12-16 08:53:21 -0500

    added exporting header

---
+++ include/echo/numeric_array.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <echo/numeric_array/numeric_array.h>
+#include <echo/numeric_array/numeric_array_view.h>
+#include <echo/numeric_array/expression.h>
+#include <echo/numeric_array/numeric_array_executer.h>
+
+namespace echo {
+
+using echo::numeric_array::NumericArray;
+using echo::numeric_array::NumericArrayView;
+using echo::numeric_array::NumericArrayExecuter;
+
+} //end namespace echo
